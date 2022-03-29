ea1607adc4eb3682969e2a8cd068c56204f5c51f
TNT05 <62080092+TNT05@users.noreply.github.com>
2022-03-29 17:59:32 -0400

    forced fail

--- src/test/java/soen/tutorial/ArithmeticOperationTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationTest.java
@@ -3,8 +3,10 @@ package soen.tutorial;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class ArithmeticOperationTest {
+	
 
 
 	@Test
@@ -14,7 +16,8 @@ public class ArithmeticOperationTest {
 		ArithmeticOperation operations = new ArithmeticOperation();
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
-		assertEquals(expected, actual);
+		//assertEquals(expected, actual);
+		fail("must fail");
 
 	}
    /* @Test
